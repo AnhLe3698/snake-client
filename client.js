41e7e0effe79762a025142d11bba0837510ae18f
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-22 21:14:32 -0600

    adding name

--- client.js
+++ client.js
@@ -9,12 +9,13 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  /*
+  
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Successfully Connected!");
+    conn.write("Name: AVL");
   });
-  */
+  
   conn.on("data", (data) => {
     // code that does something when the connection is first established
     console.log(data.toString());
